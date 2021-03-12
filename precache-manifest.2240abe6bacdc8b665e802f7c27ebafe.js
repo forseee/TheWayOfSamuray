54d90a5bfc2e5d8a2fb77a4a099b7e451bfa3e0d
forseee <57754199+forseee@users.noreply.github.com>
2021-03-12 15:43:09 +0400

    Updates

---
+++ precache-manifest.2240abe6bacdc8b665e802f7c27ebafe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9903ac0f33a8b7dd9134d6e71295108b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c308b61f9d614f94706",
+    "url": "/static/css/3.a07f04f3.chunk.css"
+  },
+  {
+    "revision": "42e1838118fdaed766c3",
+    "url": "/static/css/4.4d62e689.chunk.css"
+  },
+  {
+    "revision": "32853e6bfbb43fdfcf5f",
+    "url": "/static/css/main.8bb76342.chunk.css"
+  },
+  {
+    "revision": "9c3fd647fe36a4a2f61c",
+    "url": "/static/js/2.721583a2.chunk.js"
+  },
+  {
+    "revision": "7410b1c846886bcecbd03609e529eb8d",
+    "url": "/static/js/2.721583a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c308b61f9d614f94706",
+    "url": "/static/js/3.c9e801ed.chunk.js"
+  },
+  {
+    "revision": "42e1838118fdaed766c3",
+    "url": "/static/js/4.4b8a9524.chunk.js"
+  },
+  {
+    "revision": "32853e6bfbb43fdfcf5f",
+    "url": "/static/js/main.2b5312f3.chunk.js"
+  },
+  {
+    "revision": "988d5392f95f9c61b046",
+    "url": "/static/js/runtime-main.222911ad.js"
+  },
+  {
+    "revision": "358377a4a2d056765e2e448f7e16a6a9",
+    "url": "/static/media/Spinner-1.5s-224px.358377a4.svg"
+  }
+]);
